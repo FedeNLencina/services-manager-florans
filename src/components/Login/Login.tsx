90a75c49446a9ed0b6431dfdb95d6628590d4570
Federico Lencina <federiconahuellencina@gmail.com>
2025-01-23 20:55:13 -0300

    finish login modal desing

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./login.css";
+import { Link } from "react-router";
 
 export const Login = () => {
   return (
@@ -103,40 +104,11 @@ export const Login = () => {
           </div>
 
           <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-          <div className="form-floating">
-            <input
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="name@example.com"
-            />
-            <label htmlFor="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input
-              type="password"
-              className="form-control"
-              id="floatingPassword"
-              placeholder="Password"
-            />
-            <label htmlFor="floatingPassword">Password</label>
-          </div>
-
-          <div className="form-check text-start my-3">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="remember-me"
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" htmlFor="flexCheckDefault">
-              Remember me
-            </label>
-          </div>
-          <button className="btn btn-primary w-100 py-2" type="submit">
-            Sign in
-          </button>
+          <Link to="/">
+            <button className="btn btn-primary w-100 py-2" type="submit">
+              Sign in
+            </button>
+          </Link>
           <p className="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
         </form>
       </main>
