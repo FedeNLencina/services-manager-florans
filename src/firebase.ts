4e4f32674175e1a12b9bf72b931f3cf6efea9771
Federico Lencina <federiconahuellencina@gmail.com>
2025-02-05 23:47:56 -0300

    add react google library

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGD0jQ3FZTnbEbvDAKZ5pgS2W66JO3N-Q",
+  authDomain: "services-incomming-register.firebaseapp.com",
+  projectId: "services-incomming-register",
+  storageBucket: "services-incomming-register.firebasestorage.app",
+  messagingSenderId: "176049711316",
+  appId: "1:176049711316:web:266aeb3bf10243557bb341"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
+console.log("client id: ", clientId);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <GoogleOAuthProvider clientId={clientId}>
+        <App />
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
