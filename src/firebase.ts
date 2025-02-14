0c018d3bdf5078723e528940204932334946ea7b
Federico Lencina <federiconahuellencina@gmail.com>
2025-02-14 20:46:33 -0300

    add api key from google cloud

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,7 +6,7 @@ import { getAuth, GoogleAuthProvider, signInWithCredential } from "firebase/auth
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:"import.meta.env.FIREBASE_API_KEY",
+  apiKey:`${import.meta.env.FIREBASE_API_KEY}`,
   authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
