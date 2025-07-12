8ff5f8ee763f429f4d0992ba1a3d6def54d00980
Federico <federiconahuellencina@gmail.com>
2025-07-12 15:02:12 -0300

    add database property

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -8,49 +8,52 @@ import { ServiceSelectionContainer } from "./ServiceSelection/ServiceSelectionCo
 import { ShowTotalPrice } from "./ShowTotalPrice.tsx/ShowTotalPrice";
 import { ServiceCreationProvider } from "../../context/ServiceCreationContext";
 
+export const getDatabaseName = (email: string) => {
+  let databaseName;
+  if (email === "florancemelina@gmail.com") {
+    databaseName = "melTable";
+  } else if (email === "federiconahuellencina@gmail.com") {
+    databaseName = "fedeTest";
+  } else {
+    databaseName = "";
+  }
+
+  return databaseName;
+};
+
 export const Dashboard = () => {
   const emails = useEmailDocument();
   const user = useAuth();
   const emailAlreadyExists = emails.find((email) => email == user?.email);
 
-  const getDatabaseName = (email: string) => {
-    let databaseName;
-    if (email === "florancemelina@gmail.com") {
-      databaseName = "melTable";
-    } else if (email === "federiconahuellencina@gmail.com") {
-      databaseName = "fedeTest";
-    } else {
-      databaseName = "";
-    }
-
-    return databaseName;
-  };
-
   const databaseName =
     emailAlreadyExists != undefined ? getDatabaseName(emailAlreadyExists) : "";
 
   const userTableInfo = useGetTableDocument(databaseName);
- console.log("user table: ", userTableInfo);
- return (
-   <>
-     {!user ? (
-       <>
-         <NotLoggedUser></NotLoggedUser>
-       </>
-     ) : (
-       <>
-         <UserInfo></UserInfo>
-         <ServiceCreationProvider>
-           <ServiceSelectionContainer
-             tableInfo={userTableInfo}
-             databaseName={databaseName}
-           ></ServiceSelectionContainer>
+  console.log("user table: ", userTableInfo);
+  return (
+    <>
+      {!user ? (
+        <>
+          <NotLoggedUser></NotLoggedUser>
+        </>
+      ) : (
+        <>
+          <UserInfo></UserInfo>
+          <ServiceCreationProvider>
+            <ShowTotalPrice tableInfo={userTableInfo ? userTableInfo : []} />
+            <ServiceSelectionContainer
+              tableInfo={userTableInfo}
+              databaseName={databaseName}
+            ></ServiceSelectionContainer>
 
-           <Table tableInfo={userTableInfo}></Table>
-         </ServiceCreationProvider>
-         <ShowTotalPrice tableInfo={userTableInfo ? userTableInfo : []} />
-       </>
-     )}
-   </>
- );
+            <Table
+              databaseName={databaseName}
+              tableInfo={userTableInfo}
+            ></Table>
+          </ServiceCreationProvider>
+        </>
+      )}
+    </>
+  );
 };
--- src/components/Dashboard/Table/Table.tsx
+++ src/components/Dashboard/Table/Table.tsx
@@ -1,9 +1,16 @@
+import { deleteDoc, doc } from "firebase/firestore";
 import { formatCurrency } from "../../../utils/formatARGPrice";
+import { db } from "../../../firebase";
 
 interface TableInfoProps {
   tableInfo: any[];
+  databaseName: string;
 }
-export const Table = ({ tableInfo }: TableInfoProps) => {
+export const Table = ({ tableInfo, databaseName }: TableInfoProps) => {
+  const deleteService = async (id: string) => {
+    await deleteDoc(doc(db, databaseName, id));
+  };
+
   return (
     <div className="container text-center">
       <table className="table">
@@ -18,6 +25,7 @@ export const Table = ({ tableInfo }: TableInfoProps) => {
         </thead>
         <tbody>
           {tableInfo?.map((info, index) => {
+            console.log("info: ", info);
             return (
               <tr key={index}>
                 <th scope="row">
@@ -27,6 +35,9 @@ export const Table = ({ tableInfo }: TableInfoProps) => {
                 <td>{info.serviceName}</td>
                 <td>{formatCurrency(parseInt(info.price))}</td>
                 <td>{info.payMethod}</td>
+                <td onClick={() => deleteService(info.id)}>
+                  <i className="fa-solid fa-trash"></i>
+                </td>
               </tr>
             );
           })}
